41868c169568a17c1e95b8cdeb49653d1729661b
yangzhenyuan <yangzhenyuan@vipkid.com.cn>
2020-10-29 17:14:44 +0800

    2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
         //     ["/pages/flow.md", "思路"],
         // ],
         {
-          "/blog/": [
+          "/pages/": [
             {
               title: "Node",
               collapsable: false,
